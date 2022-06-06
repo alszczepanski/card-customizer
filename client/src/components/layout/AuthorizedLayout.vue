e96edcccd606c3ba35f02c9047d773101d5f3236
Maks Swierczynski <maksswierczynski@gmail.com>
2022-06-06 20:50:44 +0200

    Add current user to toolbar

--- client/src/components/layout/AuthorizedLayout.vue
+++ client/src/components/layout/AuthorizedLayout.vue
@@ -1,28 +1,28 @@
 <script>
-
-import LContent from './LContent.vue';
-import LToolbar from './LToolbar.vue';
-import LNavigation from './LNavigation.vue';
-import styles from './AuthorizedLayout.module.css';
+import LContent from "./LContent.vue";
+import LToolbar from "./LToolbar.vue";
+import LNavigation from "./LNavigation.vue";
+import styles from "./AuthorizedLayout.module.css";
+import UserInfo from "./UserInfo.vue";
 
 export default {
-    components: { LContent, LToolbar, LNavigation },
-    data() {
-        return {
-            styles,
-        };
-    },
+  components: { LContent, LToolbar, LNavigation, UserInfo },
+  data: () => {
+    return {
+      styles,
+    };
+  },
 };
 </script>
 
 <template>
-    <div :class="styles.layout">
-        <LToolbar><h1>Kreator wizytówek</h1></LToolbar>
-        <div :class="styles.mainContent">
-            <LNavigation :class="styles.sider">Sider</LNavigation>
-            <LContent :class="styles.content">
-                <slot />
-            </LContent>
-        </div>
+  <div :class="styles.layout">
+    <LToolbar><h1>Kreator wizytówek</h1> <UserInfo /></LToolbar>
+    <div :class="styles.mainContent">
+      <LNavigation :class="styles.sider">Sider</LNavigation>
+      <LContent :class="styles.content">
+        <slot />
+      </LContent>
     </div>
+  </div>
 </template>
---
+++ client/src/components/layout/UserInfo.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+import useAuthStore from "@/stores/authStore";
+import styles from './UserInfo.module.css';
+
+const router = useRouter();
+
+const { user, logout } = useAuthStore();
+const handlelogout = () => {
+    logout();
+    router.replace("/login");
+}
+</script>
+
+<template>
+  <div :class="styles.container">
+    <div>
+        Zalogowano jako: {{ user.username }}
+    </div>
+    <button class="btn btn-light" @click="handlelogout">Wyloguj się</button>
+  </div>
+</template>
