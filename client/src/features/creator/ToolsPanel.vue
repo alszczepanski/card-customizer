d873fa6b0e829fe2a0410c53e4a2189cec60f916
Maks Swierczynski <maksswierczynski@gmail.com>
2022-06-10 13:56:00 +0200

    ToolsPanel - add styles

--- client/src/features/creator/ToolsPanel.vue
+++ client/src/features/creator/ToolsPanel.vue
@@ -1,12 +1,18 @@
 <template>
     <div :class="styles.toolsPanel">
-        <h1>Tools Panel</h1>
+        <h1>Narzędzia</h1>
+
+        <h6>Przeciągnij zdjęcie wizytówki</h6>
         <DraggableNode :item="{ type: 'photo' }" />
+
+        <h6>Przeciągnij tekst wizytówki</h6>
         <DraggableNode :item="{ type: 'text' }" />
 
-        <label for="colorInput" class="form-label">Kolor wizytówki</label>
-        <input type="color" class="form-control form-control-color" id="colorInput" value="#ffffff"
-            @change="handleSetColor" />
+        <div :class="styles.colorPicker">
+            <label for="colorInput" class="form-label">Ustaw kolor wizytówki</label>
+            <input type="color" class="form-control form-control-color" id="colorInput" value="#ffffff"
+                @change="handleSetColor" />
+        </div>
     </div>
 </template>
 
